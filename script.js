6beb83846d91e2d6a59100e4c37efd3c73c832cd
Majid <132278743+ThunderEon@users.noreply.github.com>
2024-02-21 17:19:59 +0330

    the end

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const selectMenu = document.querySelectorAll("select");
 const time = document.querySelector(".container h1");
+const alarmBtn = document.querySelector(".container button");
+const content = document.querySelector(".content");
+
+const rington = new Audio("./Files/Good Morning Alarm.mp3");
+
+let alarmTime;
+let setAlarm = false;
+
 for (let i = 0; i <= 23; i++) {
   i = i < 10 ? "0" + i : i;
   let option = `<option value="${i}">${i}</option>`;
@@ -24,6 +32,32 @@ const currentTime = () => {
     s = s < 10 ? "0" + s : s;
 
     time.innerHTML = `${h}:${m}:${s}`;
+
+    if (alarmTime == `${h}:${m}`) {
+      rington.play();
+      rington.loop = true;
+    }
   }, 1000);
 };
 currentTime();
+
+alarmBtn.addEventListener("click", () => {
+  alarmTime = `${selectMenu[0].value}:${selectMenu[1].value}`;
+  if (alarmTime.includes("hour") || alarmTime.includes("minute")) {
+    return alert("the time is not correct");
+  }
+  setAlarm = !setAlarm;
+  disableBtn(setAlarm);
+  rington.pause();
+});
+
+const disableBtn = () => {
+  if (setAlarm) {
+    content.classList.add("disable");
+    alarmBtn.innerHTML = "clear alarm";
+  } else {
+    alarmTime = "";
+    content.classList.remove("disable");
+    alarmBtn.innerHTML = "set alarm";
+  }
+};
